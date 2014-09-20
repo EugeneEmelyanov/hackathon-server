e6d5a7f86bda9b9e571d93a0f4934be2a7eb9b23
unknown <Yauhen_Yemelyanau@EPUSPRIW0374.princeton.epam.com>
2014-09-20 00:03:33 -0700

    Fixes

--- server.js
+++ server.js
@@ -11,10 +11,13 @@ app.phase(locomotive.boot.views());
 // start an HTTP server.  Additional phases can be inserted as needed, which
 // is particularly useful if your application handles upgrades from HTTP to
 // other protocols such as WebSocket.
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 app.phase(require('bootable-environment')(__dirname + '/config/environments'));
 app.phase(bootable.initializers(__dirname + '/config/initializers'));
 app.phase(locomotive.boot.routes(__dirname + '/config/routes'));
-app.phase(locomotive.boot.httpServer(3000, '0.0.0.0'));
+app.phase(locomotive.boot.httpServer(server_port, server_ip_address));
 
 // Boot the application.  The phases registered above will be executed
 // sequentially, resulting in a fully initialized server that is listening
