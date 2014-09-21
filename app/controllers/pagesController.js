bc4cc150025c18ae26c23915e93fba9ea4bc92ef
unknown <Yauhen_Yemelyanau@EPUSPRIW0374.princeton.epam.com>
2014-09-21 15:42:57 -0700

    Fake commit

--- app/controllers/pagesController.js
+++ app/controllers/pagesController.js
@@ -1,81 +1,9 @@
-var locomotive = require('locomotive');
-var Controller = locomotive.Controller;
-var ProjectModel = require('../model/project_model');
-var util = require('../model/utils');
-
-var PagesController = new Controller();
-
-PagesController.main = function() {
-    this.render('index');
-};
-
-PagesController.runTest = function() {
-    var self = this;
-    var id = this.param("id");
-    utils.__performTest(id, function(report) {
-        ProjectModel.findOne({_id:id}).exec(function(err, item) {
-            item = item.toObject();
-            item.reportUrl = self.__req.protocol + '://' + self.__req.get('host') + "/" + item._id + "/";
-            self.__res.send(item);
-        });
-    });
-};
-
-PagesController.getProjectInfo = function() {
-    var id = this.param("id"),
-        self = this;
-    ProjectModel.findOne({_id: id}).exec(function(err, item) {
-        item = item.toObject();
-        item.reportUrl = "/" + item._id + "/";
-        self.__res.send(item);
-    });
-};
-
-PagesController.create = function() {
-    console.log(this.__req.body);
-    var url = this.__req.body.url,
-        name = this.__req.body.name;
-    var self = this;
-
-    ProjectModel.create({name:name, url:url, modificationDate: new Date(), creationDate: new Date()}, function(err, item) {
-        item = item.toObject();
-        item.reportUrl = self.__req.protocol + '://' + self.__req.get('host') + "/" + item._id + "/";
-        self.__res.send({success:true,
-                         item: item});
-        utils.__performTest(item._id, function(report){
-            //todo:do something here
-        });
-    });
-};
-
-PagesController.list = function() {
-    var page = this.param('page');
-    var self = this;
-    page = page || 1;
-
-    utils.__noCache(self.__res);
-    ProjectModel.find({})
-        .skip((page-1)*20)
-        .limit(20)
-        .exec(function(err, items) {
-            var res = items.map(function(item) {
-                item = item.toObject();
-                item.reportUrl = self.__req.protocol + '://' + self.__req.get('host') + "/" + item._id + "/";
-                return item;
-            })
-            self.__res.send(res);
-        })
-};
-
-PagesController.delete = function() {
-    var id = this.param("id");
-    var self = this;
-    ProjectModel.findOne({_id: id}).remove(function(err) {
-        self.__res.send({err:err});
-    });
-};
-
-
-
-
-module.exports = PagesController;
+module.exports = function() {
+    // Define custom MIME types.  Consult the mime module [documentation](https://github.com/broofa/node-mime)
+    // for additional information.
+    /*
+     this.mime.define({
+     'application/x-foo': ['foo']
+     });
+     */
+}
