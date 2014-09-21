7046cfc651e0c73cecaf1a979c981c26323f23ae
unknown <Yauhen_Yemelyanau@EPUSPRIW0374.princeton.epam.com>
2014-09-20 21:21:24 -0700

    OpenShift fixes

--- config/initializers/04_mongoose.js
+++ config/initializers/04_mongoose.js
@@ -1,4 +1,19 @@
 module.exports = function() {
     this.mongoose = require('mongoose');
-    this.mongoose.connect('mongodb://localhost/hackaton-server');
+    this.mongoose.connect(getConnectionString());
+}
+
+function getConnectionString() {
+    // default to a 'localhost' configuration:
+    var connection_string = '127.0.0.1:27017/eugeneandrew';
+// if OPENSHIFT env variables are present, use the available connection info:
+    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+        connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+            process.env.OPENSHIFT_APP_NAME;
+    }
+
+    return connection_string;
 }
