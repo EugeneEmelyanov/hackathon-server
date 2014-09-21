25aa5a6fd5f5c060bb64f252c3a9420fbe4736a9
unknown <Yauhen_Yemelyanau@EPUSPRIW0374.princeton.epam.com>
2014-09-20 22:31:11 -0700

    OpenShift final fix

--- app/controllers/pagesController.js
+++ app/controllers/pagesController.js
@@ -6,15 +6,79 @@ var glob = require("glob");
 var mkdirp = require('mkdirp');
 var ProjectModel = require('../model/project_model');
 
-var PROJECT_SOURCES = "/../public/sources";
-var PROJECT_REPORTS = "/";
-
 var PagesController = new Controller();
+
+PagesController.main = function() {
+    this.render('index');
+}
 
 PagesController.runTest = function() {
     var self = this;
     var id = this.param("id");
+    self.__performTest(id, function(report) {
+        ProjectModel.findOne({_id:id}).exec(function(err, item) {
+            item.reportUrl = self.__req.protocol + '://' + self.__req.get('host') + "/" + item._id + "/";
+            self.__res.send(item);
+        });
+    })
+};
+
+PagesController.getProjectInfo = function() {
+    var id = this.param("id"),
+        self = this;
+    ProjectModel.findOne({_id: id}).exec(function(err, item) {
+        item = item.toObject();
+        item.reportUrl = "/" + item._id + "/";
+        self.__res.send(item);
+    });
+};
+
+PagesController.create = function() {
+    console.log(this.__req.body);
+    var url = this.__req.body.url,
+        name = this.__req.body.name;
+    var self = this;
+
+    ProjectModel.create({name:name, url:url}, function(err, item) {
+        self.__res.send({success:true,
+                         item: item});
+        self.__performTest(item._id, function(report){
+            //todo:do something here
+        });
+    });
+};
+
+PagesController.list = function() {
+    var page = this.param('page');
+    var self = this;
+    page = page || 1;
+
+    this.__noCache(self.__res);
+    ProjectModel.find({})
+        .skip((page-1)*20)
+        .limit(20)
+        .exec(function(err, items) {
+            var res = items.map(function(item) {
+                item = item.toObject();
+                item.reportUrl = self.__req.protocol + '://' + self.__req.get('host') + "/" + item._id + "/";
+                return item;
+            })
+            self.__res.send(res);
+        })
+};
+
+PagesController.delete = function() {
+    var id = this.param("id");
+    var self = this;
+    ProjectModel.findOne({_id: id}).remove(function(err) {
+        self.__res.send({err:err});
+    });
+};
+
+PagesController.__performTest = function(id, callback) {
+    var self = this;
     ProjectModel.findOne({_id:id}).exec(function(err, item) {
+        item = item.toObject();
         var url = item.url;
         mkdirp("/../public/" + item._id + "/sources", function(err) {
             var download = new Download({ extract: true, strip: 1 })
@@ -34,23 +98,7 @@ PagesController.runTest = function() {
 
                     } else {
                         var outputDir = './public/' + item._id;
-
-                        // null options for this example
-                        var options = {
-                            title: 'Your title here'
-                        };
-
-                        var callback = function (report){
-                            // once done the analysis,
-                            // execute this
-                            var res = {};
-                            res._id = item._id;
-                            res.name = item.name;
-                            res.url = item.url;
-                            res.reportUrl = self.__req.protocol + '://' + self.__req.get('host') + "/" + item._id + "/";
-                            self.__res.send(res);
-                        };
-
+                        var options;
                         plato.inspect(files, outputDir, {}, callback);
                     }
                 });
@@ -58,63 +106,7 @@ PagesController.runTest = function() {
         });
 
     });
-
-};
-
-PagesController.getProjectInfo = function() {
-    var id = this.param("id"),
-        self = this;
-    ProjectModel.findOne({_id: id}).exec(function(err, item) {
-        var res = {};
-        res._id = item._id;
-        res.name = item.name;
-        res.url = item.url;
-        res.reportUrl = "/" + item._id + "/";
-        self.__res.send(res);
-    });
-};
-
-PagesController.create = function() {
-    console.log(this.__req.body);
-    var url = this.__req.body.url,
-        name = this.__req.body.name;
-    var self = this;
-
-    ProjectModel.create({name:name, url:url}, function(err, item) {
-        self.__res.send({success:true,
-                         item: item});
-    });
-};
-
-PagesController.list = function() {
-    var page = this.param('page');
-    var self = this;
-    page = page || 1;
-
-    this.__noCache(self.__res);
-    ProjectModel.find({})
-        .skip((page-1)*20)
-        .limit(20)
-        .exec(function(err, items) {
-            var res = items.map(function(item) {
-                var i = {};
-                i._id = item._id;
-                i.name = item.name;
-                i.url = item.url;
-                i.reportUrl = self.__req.protocol + '://' + self.__req.get('host') + "/" + item._id + "/";
-                return i
-            })
-            self.__res.send(res);
-        })
-};
-
-PagesController.delete = function() {
-    var id = this.param("id");
-    var self = this;
-    ProjectModel.findOne({_id: id}).remove(function(err) {
-        self.__res.send({err:err});
-    });
-};
+}
 
 PagesController.__noCache = function(res) {
     res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
--- app/model/project_model.js
+++ app/model/project_model.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ProjectSchema = new Schema({
-
     url: { type: String, required: true },
     name: { type: String, required: true }
 });
